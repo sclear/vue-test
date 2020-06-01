a693f7604687c53d43288621b0698b8fd832ff0b
jc <1392227049@qq.com>
2020-06-01 17:08:40 +0800

    添加可测试组件

--- src/components/count.vue
+++ src/components/count.vue
@@ -1,26 +1,43 @@
 <template>
-  <div class="container">
-      {{value}}
+  <div class="container lj">
+      <span>{{sum}}</span>
       <button class="add" @click="change('+')">+</button>
       <button class="rd" @click="change('-')">-</button>
+      <sb></sb>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    sb: ()=> import('./subs')
+  },
   props: {
       value: {
           type: Number,
           default: ()=> 0
       }
   },
+  created() {
+    this.eimtChange()
+  },
   data() {
     return {
+      sum: 0
+    }
+  },
+  watch: {
+    value(n, o) {
+      if(n !== this.sum) this.eimtChange
     }
   },
   methods: {
       change(type) {
-          type === '+' ? this.$emit('input', this.value+1) : this.$emit('input', this.value-1)
+          this.sum = type === '+' ? this.sum + 1 : this.sum - 1;
+          type === '+' ? this.$emit('input', this.value+1) : this.$emit('input', this.value-1);
+      },
+      eimtChange(){
+        if(this.value || this.value === 0) this.sum = this.value
       }
   }
 }
---
+++ src/components/subs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sb">
+      sb
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
